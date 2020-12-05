f20e44c6d4f49f93b4611091cd425f205e589efb
gracenikole <grace.alvarado@utec.edu.pe>
2020-12-05 13:46:58 -0500

    :frog: inputs and outputs added

--- fp_adder.v
+++ fp_adder.v
@@ -1,5 +1,12 @@
-module fp_adder (
-  
-);
+module fp_adder (srcA, srcB);
+input [31:0] srcA;
+input [31:0] srcB;
+
+output [31:0] result;
+output [3:0] ALUFlags;
+
+wire N, Z, C, V;
+
+
 
 endmodule //fp_adder
